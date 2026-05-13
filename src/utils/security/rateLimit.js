7723f09b7c13baa5e591a3a72396aeac5fa1eb5d
Omda112 <emadehab467@gmail.com>
2026-05-13 15:33:31 +0300

    Limit-increase

--- src/utils/security/rateLimit.js
+++ src/utils/security/rateLimit.js
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit"
 
 export const rateLimiter = rateLimit({
-    limit: 20,
+    limit: 100,
     windowMs: 60*1000,
     handler: (req,res,next)=>{
         return next(new Error("Too many requests, Please try again in a minute", {cause: 429}))
